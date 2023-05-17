578b27e8d404ef5a865589617fb4d0a1dc875f2d
WesleyT <wtopping@gmail.com>
2023-05-16 22:58:46 -0500

    Version is supposed to be 2.2

--- SnapStats_StreamlabsSystem.py
+++ SnapStats_StreamlabsSystem.py
@@ -8,7 +8,7 @@ ScriptName = "Snap Stats"
 Website = "https://www.weazol.com"
 Description = "A Script to assist with displaying snap stats in OBS"
 Creator = "Weazol"
-Version = "2.1"
+Version = "2.2"
 
 settingsFile = "settings.json"
 settings = {}
